f30f6803327c9fbeca18077a0b74fcd6f300b36d
Thomas Buckley <buckleythomas92@gmail.com>
2024-10-21 00:01:49 +0900

    begin file entity

---
+++ src/components/files/files_entity.ts
@@ -0,0 +1,41 @@
+import { Tasks } from "components/tasks/tasks_entity";
+import { Users } from "components/users/users_entity";
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  Entity,
+} from "typeorm";
+
+@Entity()
+export class Files {
+  @PrimaryGeneratedColumn("uuid")
+  file_id: string;
+
+  @Column({ length: 30, nullable: false, unique: true })
+  file_name: string;
+
+  @Column({ length: 30 })
+  mime_type: string;
+
+  @Column()
+  @ManyToOne(() => Users, (userData) => userData.user_id)
+  @JoinColumn({ name: "user_id" })
+  created_by: string;
+
+  @Column()
+  @ManyToOne(() => Tasks, (taskData) => taskData.task_id)
+  @JoinColumn({ name: "task_id" })
+  task_id: string;
+
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column()
+  @CreateDateColumn()
+  updated_at: Date;
+}
